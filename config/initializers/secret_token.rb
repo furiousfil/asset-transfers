6b2dda9e31cc89b9d72cf16e358e47e2b3293cc2
Phil <phil@phuriosa.com>
2014-01-05 15:54:13 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssetTransfers::Application.config.secret_key_base = '2741f10c030142862069e1adab6a621816a3aaf9ce05e288c365c74f5ab78c17a8cc05f00caff0faa71c719a0a19d0f9d4f734f6deb64cd0b911b643ccc18cf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssetTransfers::Application.config.session_store :cookie_store, key: '_asset-transfers_session'
